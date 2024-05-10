0039346ca43e6c46e0272a625327e48132d0521c
mclaramarinho <marinho.claramb@gmail.com>
2024-05-10 13:24:52 -0300

    redirect to tasks index + changed nav item to board view

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ToDoList.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Tasks");
         }
 
         public IActionResult Privacy()
