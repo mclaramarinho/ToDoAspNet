fad7304658ddaa57840223feaefd403c55f2dab8
mclaramarinho <marinho.claramb@gmail.com>
2024-05-14 20:24:58 -0300

    FEAT: created model for User table

---
+++ ToDoList/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace ToDoList.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(80)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8), MaxLength(20)]
+
+        public string Password { get; set; }
+
+        public ICollection<TaskItem> tasks = new List<TaskItem>();
+    }
+}
