0864584c46748d89b21b96049d726c568026282b
mclaramarinho <marinho.claramb@gmail.com>
2024-05-14 20:22:19 -0300

    FEAT: created Users table + added Unique constraint to email column in table Users

--- ToDoList/Data/ApplicationDbContext.cs
+++ ToDoList/Data/ApplicationDbContext.cs
@@ -11,13 +11,14 @@ namespace ToDoList.Data
         }
 
         public DbSet<TaskItem> TaskItems { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TaskItem>()
                 .Property(t => t.StartDate)
                 .HasColumnName("StartDate");
-            
+
             modelBuilder.Entity<TaskItem>()
                 .Property(t => t.EndDate)
                 .HasColumnName("EndDate");
@@ -26,8 +27,12 @@ namespace ToDoList.Data
                 .Property(t => t.Status)
                 .HasColumnName("Status");
 
+            modelBuilder.Entity<User>()
+                .HasIndex(t => t.Email).IsUnique();
+
             base.OnModelCreating(modelBuilder);
         }
+
 
 
     }
