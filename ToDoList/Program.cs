6178dd46474f5d95c52902877f0bba0db7713f62
mclaramarinho <marinho.claramb@gmail.com>
2024-05-14 20:29:09 -0300

    FEAT: registered Session + HttpContextAccessor + TaskService + UsersService + AccessControlService

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,16 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Data;
+using ToDoList.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddSession(o =>
+{
+    o.IdleTimeout = TimeSpan.FromSeconds(1800);
+    o.Cookie.IsEssential = true;
+    o.Cookie.HttpOnly = true;
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddTransient<ITaskService, TaskService>();
+builder.Services.AddTransient<IUsersServices,  UsersService>();
+builder.Services.AddTransient<IAccessControlService, AccessControlService>();
+
 var app = builder.Build();
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
